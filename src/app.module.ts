f8c6fe844fb2fea0ad13d9326a17498999beac21
AhmedAymanBfcai <ahmedaymanbfcai123@gmail.com>
2024-05-23 11:47:00 +0300

    Persisting Data With TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,19 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
+import { User } from './users/users.entity';
+import { Report } from './reports/report.entity';
 
 @Module({
-  imports: [UsersModule, ReportsModule],
+  imports: [ TypeOrmModule.forRoot({
+    type: 'sqlite',
+    database: 'db.sqlite',
+    entities: [User, Report],
+    synchronize: true, //Only for use in dev mode. Automatic migration stuff..
+  }),UsersModule, ReportsModule],
   controllers: [AppController],
   providers: [AppService],
 })
