a4a1c9c525ca0aac9e5590230a4b778b3655914b
AhmedAymanBfcai <ahmedaymanbfcai123@gmail.com>
2024-05-24 01:40:21 +0300

    Quering, Updating, and Removing functions

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Get, Patch, Param, Query } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user-dto';
 import { UsersService } from './users.service';
 
@@ -10,4 +10,24 @@ export class UsersController {
     createUser(@Body() body: CreateUserDto) {
         this.userService.create(body.email, body.password)
     }
+
+    @Get('/:id')
+    findUser(@Param('id') id: string) {
+        return this.userService.findOne(parseInt(id)); // As nest deal with the number in the url as a string.
+    }
+
+    // @Post('/signup')
+    // createUser(@Body() body: CreateUserDto) {
+    //     this.userService.create(body.email, body.password)
+    // }
+
+    // @Post('/signup')
+    // createUser(@Body() body: CreateUserDto) {
+    //     this.userService.create(body.email, body.password)
+    // }
+
+    // @Post('/signup')
+    // createUser(@Body() body: CreateUserDto) {
+    //     this.userService.create(body.email, body.password)
+    // }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -17,4 +17,34 @@ export class UsersService {
 
         return this.repo.save(user);
     }
+
+    findOne(id: number) {
+       return this.repo.findOneBy({id})
+    }
+
+    find(email: string){
+       return this.repo.findBy({ email })
+    }
+
+    // findOneBy({where : {id: id}})
+    async update(id: number, attributes: Partial<User>){
+        // Partial is a type helper tells us that attrs can be any object that has at least or none some of the properities of the user class. 
+        const user = await this.findOne(id);
+        if (!user){
+            throw new Error('User not found');
+        }
+
+        Object.assign(user, attributes);
+
+        return this.repo.save(user);
+    }
+
+    async remove(id: number){
+        const user = await this.findOne(id);
+        if(!user){
+            throw new Error("User not found");
+        }
+
+        return this.repo.remove(user);
+    }
 }
